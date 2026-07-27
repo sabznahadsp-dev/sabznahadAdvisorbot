9977718be51bb9c72125226ee7eddb67ea3dde06
Sabznahad <sabznahadsp@gmail.com>
2026-07-27 15:31:55 +0330

    Fix async database connection imports

--- app/database/__init__.py
+++
@@ -1,2 +0,0 @@
-from app.database.base import Base
-from app.database.connection import get_db
--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
+    async_sessionmaker,
     create_async_engine,
-    async_sessionmaker
 )
 
 from app.core.config import settings
@@ -9,21 +9,17 @@ from app.core.config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
-    echo=False
+    echo=False,
 )
 
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
-    expire_on_commit=False
+    expire_on_commit=False,
 )
 
 
 async def get_db():
-    """
-    Database session generator
-    """
-
     async with AsyncSessionLocal() as session:
         yield session
